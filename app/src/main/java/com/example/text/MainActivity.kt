82533dbe5c40bda8f5ab746e107bd58c16728858
AdiletExp <turakulov_adilet@mail.ru>
2022-09-21 17:59:49 +0600

    commit

---
+++ app/src/main/java/com/example/text/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.example.text
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.text.ui.theme.TextTheme
+import com.google.accompanist.permissions.*
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPermissionsApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TextTheme {
+                val multiplePermissionsState = rememberMultiplePermissionsState(
+                    listOf(
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                        android.Manifest.permission.CAMERA,
+                    )
+                )
+                Sample(multiplePermissionsState)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+private fun Sample(multiplePermissionsState: MultiplePermissionsState) {
+    if (multiplePermissionsState.allPermissionsGranted) {
+        // If all permissions are granted, then show screen with the feature enabled
+        Text("Camera and Read storage permissions Granted! Thank you!")
+    } else {
+        Column {
+            Text(
+                getTextToShowGivenPermissions(
+                    multiplePermissionsState.revokedPermissions,
+                    multiplePermissionsState.shouldShowRationale
+                )
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = { multiplePermissionsState.launchMultiplePermissionRequest() }) {
+                Text("Request permissions")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+private fun getTextToShowGivenPermissions(
+    permissions: List<PermissionState>,
+    shouldShowRationale: Boolean
+): String {
+    val revokedPermissionsSize = permissions.size
+    if (revokedPermissionsSize == 0) return ""
+
+    val textToShow = StringBuilder().apply {
+        append("The ")
+    }
+
+    for (i in permissions.indices) {
+        textToShow.append(permissions[i].permission)
+        when {
+            revokedPermissionsSize > 1 && i == revokedPermissionsSize - 2 -> {
+                textToShow.append(", and ")
+            }
+            i == revokedPermissionsSize - 1 -> {
+                textToShow.append(" ")
+            }
+            else -> {
+                textToShow.append(", ")
+            }
+        }
+    }
+    textToShow.append(if (revokedPermissionsSize == 1) "permission is" else "permissions are")
+    textToShow.append(
+        if (shouldShowRationale) {
+            " important. Please grant all of them for the app to function properly."
+        } else {
+            " denied. The app cannot function without them."
+        }
+    )
+    return textToShow.toString()
+}
+@Composable
+fun MainBox() {
+    val context = LocalContext.current
+    Box(modifier = Modifier.fillMaxSize()) {
+        Button(
+            modifier = Modifier
+                .size(size = 200.dp)
+                .align(alignment = Alignment.BottomCenter),
+            onClick = {
+                Toast.makeText(context, "clucked", Toast.LENGTH_SHORT).show()
+            }
+        ) {
+            Text(text = "Click THIS THEME", textAlign = TextAlign.Center)
+        }
+    }
+}
